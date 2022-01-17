cb3f0cecd590c902c1d6995d7838933808330ac4
AS $ git config --global user.email atlestroms@gmail.com <atlestroms@gmail.com>
2022-01-17 23:34:16 +0100

    ja

--- nettbutikk_tmm/gatsby-config.js
+++ nettbutikk_tmm/gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
     resolve: `gatsby-source-strapi`,
     options: {
       apiURL: `http://localhost:1337`,
-      queryLimit: 1000, // Default to 100
+      queryLimit: 10000, // Default to 100
       contentTypes: [`Produkt`],
      
       // Possibility to login with a strapi user, when content types are not publically available (optional).

fe9083b4941a1623139e314561a086eacd93d2a9
AS $ git config --global user.email atlestroms@gmail.com <atlestroms@gmail.com>
2022-01-20 07:08:51 +0100

    change

--- nettbutikk-tmm/src/pages/index.js
+++ nettbutikk-tmm/src/pages/index.js
@@ -1,8 +1,8 @@
 import * as React from "react"
-import { Link } from `gatsby`
+import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-import { graphql } from `gatsby`
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"

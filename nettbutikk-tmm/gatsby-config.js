14eda1e9b3ff321ced0267fef0f231b433a10b1c
AS $ git config --global user.email atlestroms@gmail.com <atlestroms@gmail.com>
2022-01-24 04:51:09 +0100

    i

--- nettbutikk-tmm/gatsby-config.js
+++ nettbutikk-tmm/gatsby-config.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line no-undef
-export const siteMetadata = {
+module.exports; const siteMetadata = {
   title: `Gatsby Default Starter`,
   description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
   author: `@gatsbyjs`,
 }
-export const plugins = [
+module.exports; const plugins = [
   {
     resolve: "gatsby-source-strapi",
     options: {

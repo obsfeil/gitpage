b51cc49d1daf314ca229b5b759155ad8122ada76
AS $ git config --global user.email atlestroms@gmail.com <atlestroms@gmail.com>
2022-01-24 05:00:42 +0100

    l

--- nettbutikk-tmm/src/pages/index.js
+++ nettbutikk-tmm/src/pages/index.js
@@ -1,7 +1,6 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
 import * as React from "react"
-// import { Link } from "gatsby"
-// import { StaticImage } from "gatsby-plugin-image"
+ import { Link } from "gatsby"
+ import { StaticImage } from "gatsby-plugin-image"
 
 import { graphql } from "gatsby"
 
